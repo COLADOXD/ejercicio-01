cd15f129f8d3f167d4b7c2e09add3fcb2a78f10f
embenavides <106458598+COLADOXD@users.noreply.github.com>
2023-01-11 11:32:14 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// alert("hola como estas");
+// alert("chaou");
+let age = 19;
+let name = "Mateo Sebastian";
+let sport = "Chess";
+let lastName = "Chacon Trelles";
+let country = "Ecuador";
+let estadoCivil = true;
+let suma = age + 2;
+console.log(name);
+console.log(lastName);
+console.log(sport);
+console.log(estadoCivil);
+console.log(country);
+console.log(suma);
+console.log(age);
+let array = [3, 2, 5, 7]
+let objeto = {
+    gato: "perro",
+    cazador: "presa",
+}
+console.log(array[2]);
+console.log(objeto.gato);
+
+// Tipos de Datos
+console.log(typeof(estadoCivil));
+console.log(typeof(country));
+console.log(typeof(array));
